a42437e5cfbb0c19301039ad2df090d001dea08e
carlacentenor <carlacentenor@gmail.com>
2017-11-08 19:55:57 -0500

    app culminado

--- js/app.js
+++ js/app.js
@@ -1,41 +1,56 @@
-window.onload = function(){ /*hasta que se cargue totalmente recien ejecutar la funcion*/
+var arrayGame = [];
+var board = document.querySelector(".board-js");
+var arrayCell = document.querySelectorAll(".cell");
+var centinela = true; /* variable para turno inicializada*/
+var textWinner = document.querySelector(".text-winner");
 
-var board = document.querySelector(".board-js"); /*seleccionando la primera clase .board-js*/
+board.addEventListener("click", loadGame); /*Evento que llama a funcion*/
 
-board.addEventListener("click",addX);
-/*
-board.addEventListener("click",function(event){ Hacer un cambio cuando hace doble click--event es un parametro creado
-  event.target.style.backgroundColor ="#da91ec"; cambia el color cuando seleccione
-target seleccionando solo esa etiqueta
-event.target.style.border ="2px solid #6214ba";
-event.target.textContent = ":)"; *solo agregas texto*
+function loadGame(event) {
+	var positionCell = event.target.id[1] - 1; /*obtenemos la posicion de cada celda*/
+	if (centinela) {
+		event.target.style.backgroundColor = "#00d6f4";
+		event.target.textContent = "X";
+		centinela = false;
+		arrayGame[positionCell] = "X"; /*agregamos segun la posicion seleccionada el valo "X"*/
+		winner("X");
+	} else {
+		event.target.style.backgroundColor = "#ff0183";
+		event.target.textContent = "O";
 
-})
-board.addEventListener("dblclick",function(event){
-    event.target.style.backgroundColor ="#09c953";
-    event.target.textContent = "O";
-}); ---- evento de cambiar color al pasar el mouse
-
-
-board.addEventListener("dblclick",function(event){
-  event.target.style.backgroundColor ="#09c953";
-
-event.target.style.border ="2px solid #093915";
-})*/
-
+		centinela = true;
+		arrayGame[positionCell] = "O"; /*agregamos segun la posicion seleccionada el valo "O"*/
+		winner("O");
+	}
 };
 
-var centinela = false ;
+function winner(value) { /*Funcion que evalua todas las posibles combinaciones ganadoras*/
+	if ((arrayGame[0] == value && arrayGame[1] == value && arrayGame[2] == value) ||
+		(arrayGame[0] == value && arrayGame[3] == value && arrayGame[6] == value) ||
+		(arrayGame[0] == value && arrayGame[4] == value && arrayGame[8] == value) ||
+		(arrayGame[3] == value && arrayGame[4] == value && arrayGame[5] == value) ||
+		(arrayGame[1] == value && arrayGame[4] == value && arrayGame[7] == value) ||
+		(arrayGame[2] == value && arrayGame[4] == value && arrayGame[6] == value) ||
+		(arrayGame[6] == value && arrayGame[7] == value && arrayGame[8] == value) ||
+		(arrayGame[2] == value && arrayGame[5] == value && arrayGame[8] == value)) {
 
-function addX(event){  /**LLAMAR A LA FUNCION DESDE FUERA**/
+
+		textWinner.textContent = "Jugador " + value + " gana"
 
 
-if(centinela){
-event.target.textContent = "X";
-centinela=false;
+
+
+	} else if (arrayGame[0] != null && arrayGame[1] != null && arrayGame[2] != null &&
+		arrayGame[3] != null && arrayGame[4] != null && arrayGame[5] != null &&
+		arrayGame[6] != null && arrayGame[7] != null && arrayGame[8] != null) {
+		textWinner.textContent = "Empate , juega otra vez"
+
+
+	}
+
 }
-else {
-  event.target.textContent="O";
-  centinela=true;
+
+
+function reload() { /*Funcion para reiniciar el juego*/
+	window.location.reload();
 }
-};
